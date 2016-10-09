084eaf8584fb2e7c4592d3bbf191686180d5add7
Neha Konakalla <nehakonakalla@nehas-macbook.attlocal.net>
2016-10-08 18:45:36 -0700

    add an add method to NumberBagTwo

--- src/NumberBagTwo.java
+++ src/NumberBagTwo.java
@@ -1,4 +1,17 @@
+import java.util.ArrayList;
 
 public class NumberBagTwo {
 
+	private ArrayList<Integer> list;
+	
+	private int maxValue;
+	
+	public NumberBag(int max){
+		
+		list = new ArrayList<Integer>();
+		
+		maxValue = max;
+		
+	}
+	
 }
